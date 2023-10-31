73bc10b34b48fd3a503efbc0208107a82f1597ae
sinsinnati <sinazamani9364@gmail.com>
2023-10-31 08:10:32 +0330

    [ADD]: get card api

--- gateway/routes.go
+++ gateway/routes.go
@@ -27,5 +27,6 @@ func getRoutes(handler *handlers.Repository) http.Handler {
 	mux.Get("/profile", handler.Profile)
 	mux.Post("/edit-profile" , handler.EditProfile)
 	mux.Post("/create-card", handler.CreateCard)
+	mux.Get("/get-card", handler.GetCard)
 	return mux
 }
