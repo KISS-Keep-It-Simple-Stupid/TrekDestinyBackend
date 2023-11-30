b4f5d82e1ef67773aa5738588bd332dcee1bedd6
sobhankazemi <78590935+sobhankazemi@users.noreply.github.com>
2023-11-30 20:04:31 +0330

    [ADD]: CORS for chat history

--- services/chat/go.mod
+++ services/chat/go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/go-chi/chi/v5 v5.0.10
+	github.com/go-chi/cors v1.2.1
 	github.com/gorilla/websocket v1.5.1
 	github.com/lib/pq v1.10.9
 	github.com/spf13/viper v1.17.0
--- services/chat/routes.go
+++ services/chat/routes.go
@@ -5,10 +5,19 @@ import (
 
 	"github.com/KISS-Keep-It-Simple-Stupid/TrekDestinyBackend/services/chat/handlers"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func GetRoutes(handler *handlers.Repository) http.Handler {
 	mux := chi.NewRouter()
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	mux.Get("/{id1}/{id2}", handler.Chat)
 	mux.Get("/count/{id1}/{id2}", handler.Count)
 	mux.Get("/history/{id1}/{id2}", handler.History)
